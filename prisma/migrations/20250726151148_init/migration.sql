904c1ccb43d310b5babf97879f04e4ce2c66197f
Cezar Bacanieski <bacanieskidev@gmail.com>
2025-07-26 12:31:48 -0300

    feat: add Prisma schema and migrations

---
+++ prisma/migrations/20250726151148_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateEnum
+CREATE TYPE "QuestionType" AS ENUM ('BOOLEAN', 'INPUT', 'CHECKBOX');
+
+-- CreateTable
+CREATE TABLE "quizzes" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "quizzes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "questions" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "type" "QuestionType" NOT NULL,
+    "options" TEXT[],
+    "correctAnswer" TEXT,
+    "correctAnswers" TEXT[],
+    "quizId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "questions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "questions" ADD CONSTRAINT "questions_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "quizzes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
